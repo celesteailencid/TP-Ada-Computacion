524232bda8e350861f6874f370b29ceabd489585
celesteailencid <celesteailencid@gmail.com>
2022-04-25 01:04:32 -0300

    funcionalidad modal

---
+++ funciones.js
@@ -0,0 +1,38 @@
+const precioMaquina = (array) => {
+    // recibe un array de componentes y devuelve el precio de la máquina que se puede armar con esos componentes, que es la suma de los precios de cada componente incluido
+}
+
+const cantidadVentasComponente = (componente) => {
+    // recibe un componente y devuelve la cantidad de veces que fue vendido, o sea que formó parte de una máquina que se vendió. La lista de ventas no se pasa por parámetro, se asume que está identificada por la variable ventas
+}
+
+const vendedoraDelMes = (mes, anio) => {
+    // se le pasa dos parámetros numéricos, (mes, anio) y devuelve el nombre de la vendedora que más vendió en plata en el mes. O sea no cantidad de ventas, sino importe total de las ventas. El importe de una venta es el que indica la función precioMaquina. El mes es un número entero que va desde el 1 (enero) hasta el 12 (diciembre)
+}
+
+const ventasMes = (mes, anio) => {
+    // Obtener las ventas de un mes. El mes es un número entero que va desde el 1 (enero) hasta el 12 (diciembre)
+}
+
+const ventasVendedora = (nombre) => {
+    // Obtener las ventas totales realizadas por una vendedora sin límite de fecha
+}
+
+const componenteMasVendido = () => {
+    // Devuelve el nombre del componente que más ventas tuvo historicamente. El dato de la cantidad de ventas es el que indica la función cantidadVentasComponente
+}
+
+const huboVentas = (mes, anio) => {
+    // que indica si hubo ventas en un mes determinado. El mes es un número entero que va desde el 1 (enero) hasta el 12 (diciembre)
+}
+
+const ventasSucursal = (sucursal) => {
+    // que obtiene las ventas totales realizadas por una sucursal sin límite de fecha.
+}
+
+// Las funciones ventasSucursal y ventasVendedora tienen mucho código en común, ya que es la misma funcionalidad pero trabajando con una propiedad distinta. Entonces, ¿cómo harías para que ambas funciones reutilicen código y evitemos repetir?
+
+const sucursalDelMes = (mes, anio) => {
+    // que se le pasa dos parámetros numéricos, (mes, anio) y devuelve el nombre de la sucursal que más vendió en plata en el mes. No cantidad de ventas, sino importe total de las ventas. El importe de una venta es el que indica la función precioMaquina. El mes es un número entero que va desde el 1 (enero) hasta el 12 (diciembre)
+}
+
---
+++ ventanas-modal.js
@@ -0,0 +1,42 @@
+const botonEditar = document.querySelectorAll(".boton-editar");
+const botonEliminar = document.querySelectorAll(".boton-tacho");
+const fondoModal = document.querySelector(".fondo-modal");
+const ventanaModal = document.querySelector(".ventana-modal");
+
+fondoModal.classList.add("oculto");
+ventanaModal.classList.add("oculto");
+
+// funcion para abrir una ventana modal
+const abrirModal = () => {
+    fondoModal.classList.remove("oculto");
+    ventanaModal.classList.remove("oculto");
+}
+
+const cerrarModal = () => {
+    fondoModal.classList.add("oculto");
+    ventanaModal.classList.add("oculto");
+}
+
+
+// funcionalidad de boton editar modal
+for (let i = 0; i < botonEditar.length; i++) {
+    botonEditar[i].onclick = () => {
+        console.log("funciona boton editar")
+        return abrirModal();
+    }
+};
+// funcionalidad de boton editar modal
+
+// funcionalidad de boton eliminar modal
+for (let i = 0; i < botonEliminar.length; i++) {
+    botonEliminar[i].onclick = () => {
+        console.log("funciona boton eliminar")
+        return abrirModal();
+    }
+};
+// funcionalidad de boton eliminar modal
+
+fondoModal.onclick = () => {
+    return cerrarModal();
+}
+
