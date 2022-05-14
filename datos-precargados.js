6131d2ba8ba39ac8bf5ed3677a9734529e7bfb61
celesteailencid <celesteailencid@gmail.com>
2022-05-14 12:45:55 -0300

    Ventanas modal funcionalidad btn Cancelar ok

--- datos-precargados.js
+++ datos-precargados.js
@@ -1,52 +1,40 @@
-const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"]
+const local = {
+    vendedoras: ["Ada", "Grace", "Hedy", "Sheryl"],
+    sucursales: ["Centro", "Caballito"],
 
-const ventas = [
-    [
-        1,
-        new Date(2019, 1, 4),
-        "Grace",
-        "Centro",
-        ["Monitor GPRS 3000", "Motherboard ASUS 1500"]
+    ventas: [
+        // tener en cuenta que Date guarda los meses del 0 (enero) al 11 (diciembre)
+        { fecha: new Date(2019, 1, 4), nombreVendedora: "Grace", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1500"], sucursal: "Centro" },
+        { fecha: new Date(2019, 0, 1), nombreVendedora: "Ada", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1500"], sucursal: "Centro" },
+        { fecha: new Date(2019, 0, 2), nombreVendedora: "Grace", componentes: ["Monitor ASC 543", "Motherboard MZI"], sucursal: "Centro" },
+        { fecha: new Date(2019, 0, 10), nombreVendedora: "Ada", componentes: ["Monitor ASC 543", "Motherboard ASUS 1200"], sucursal: "Centro" },
+        { fecha: new Date(2019, 0, 12), nombreVendedora: "Grace", componentes: ["Monitor GPRS 3000", "Motherboard ASUS 1200"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 24), nombreVendedora: "Sheryl", componentes: ["Motherboard ASUS 1500", "HDD Wezter Dishital"], sucursal: "Caballito" },
+        { fecha: new Date(2019, 1, 12), nombreVendedora: "Hedy", componentes: ["Monitor GPRS 3000", "HDD Toyiva"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 1), nombreVendedora: "Ada", componentes: ["Motherboard MZI", "RAM Quinston Fury"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 11), nombreVendedora: "Grace", componentes: ["Monitor ASC 543", "RAM Quinston"], sucursal: "Caballito" },
+        { fecha: new Date(2019, 1, 15), nombreVendedora: "Ada", componentes: ["Motherboard ASUS 1200", "RAM Quinston Fury"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 12), nombreVendedora: "Hedy", componentes: ["Motherboard ASUS 1500", "HDD Toyiva"], sucursal: "Caballito" },
+        { fecha: new Date(2019, 1, 21), nombreVendedora: "Grace", componentes: ["Motherboard MZI", "RAM Quinston"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 8), nombreVendedora: "Sheryl", componentes: ["Monitor ASC 543", "HDD Wezter Dishital"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 16), nombreVendedora: "Sheryl", componentes: ["Monitor GPRS 3000", "RAM Quinston Fury"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 27), nombreVendedora: "Hedy", componentes: ["Motherboard ASUS 1200", "HDD Toyiva"], sucursal: "Caballito" },
+        { fecha: new Date(2019, 1, 22), nombreVendedora: "Grace", componentes: ["Monitor ASC 543", "HDD Wezter Dishital"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 5), nombreVendedora: "Ada", componentes: ["Motherboard ASUS 1500", "RAM Quinston"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 1), nombreVendedora: "Grace", componentes: ["Motherboard MZI", "HDD Wezter Dishital"], sucursal: "Centro" },
+        { fecha: new Date(2019, 1, 7), nombreVendedora: "Sheryl", componentes: ["Monitor GPRS 3000", "RAM Quinston"], sucursal: "Caballito" },
+        { fecha: new Date(2019, 1, 14), nombreVendedora: "Ada", componentes: ["Motherboard ASUS 1200", "HDD Toyiva"], sucursal: "Centro" }
     ],
-    [
-        2,
-        new Date(2019, 0, 1),
-        "Ada",
-        "Centro",
-        ["Monitor GPRS 3000", "Motherboard ASUS 1500"]
-    ],
-    [
-        3,
-        new Date(2019, 0, 2),
-        "Grace",
-        "Caballito",
-        ["Monitor ASC 543", "Motherboard MZI"]
-    ],
-    [
-        4,
-        new Date(2019, 0, 10),
-        "Ada",
-        "Centro",
-        ["Monitor ASC 543", "Motherboard ASUS 1200"]
-    ],
-    [
-        5,
-        new Date(2019, 0, 12),
-        "Grace",
-        "Caballito",
-        ["Monitor GPRS 3000", "Motherboard ASUS 1200"]
-    ],
-]
 
-const precios = [
-    ["Monitor GPRS 3000", 200],
-    ["Motherboard ASUS 1500", 120],
-    ["Monitor ASC 543", 250],
-    ["Motherboard ASUS 1200", 100],
-    ["Motherboard MZI", 30],
-    ["HDD Toyiva", 90],
-    ["HDD Wezter Dishital", 75],
-    ["RAM Quinston", 110],
-    ["RAM Quinston Fury", 230],
-]
-
+    precios: [
+        { componente: "Monitor GPRS 3000", precio: 200 },
+        { componente: "Motherboard ASUS 1500", precio: 120 },
+        { componente: "Monitor ASC 543", precio: 250 },
+        { componente: "Motherboard ASUS 1200", precio: 100 },
+        { componente: "Motherboard MZI", precio: 30 },
+        { componente: "HDD Toyiva", precio: 90 },
+        { componente: "HDD Wezter Dishital", precio: 75 },
+        { componente: "RAM Quinston", precio: 110 },
+        { componente: "RAM Quinston Fury", precio: 230 }
+    ]
+};
--- ventanas-modal.js
+++ ventanas-modal.js
@@ -1,14 +1,22 @@
 const botonEditar = document.querySelectorAll(".boton-editar");
 const botonEliminar = document.querySelectorAll(".boton-tacho");
+const botonCancelarEditar = document.getElementById("boton-cancelar-editar");
+const botonCancelarEliminar = document.getElementById("boton-cancelar-eliminar");
 const fondoModal = document.querySelector(".fondo-modal");
 const ventanaModal = document.querySelector(".ventana-modal");
 const tituloVentanaModal = document.querySelector("#titulo-ventana-modal");
 const selectVendedora = document.querySelector(".select-vendedora");
+const fondoModalEliminar = document.querySelector(".fondo-modal-eliminar");
+const ventanaModalEliminar = document.querySelector(".ventana-modal-eliminar");
 
 fondoModal.classList.add("oculto");
 ventanaModal.classList.add("oculto");
+fondoModalEliminar.classList.add("oculto");
+ventanaModalEliminar.classList.add("oculto");
 
-// funcion para abrir una ventana modal
+
+
+// funciones para abrir y cerrar una ventana modal
 const abrirModal = () => {
     fondoModal.classList.remove("oculto");
     ventanaModal.classList.remove("oculto");
@@ -19,6 +27,19 @@ const cerrarModal = () => {
     ventanaModal.classList.add("oculto");
 }
 
+// funciones para abrir y cerrar una ventana modal eliminar
+const abrirModalEliminar = () => {
+    fondoModalEliminar.classList.remove("oculto");
+    ventanaModalEliminar.classList.remove("oculto");
+}
+
+const cerrarModalEliminar = () => {
+    fondoModalEliminar.classList.add("oculto");
+    ventanaModalEliminar.classList.add("oculto");
+}
+
+
+
 // funcionalidad de boton editar modal
 for (let i = 0; i < botonEditar.length; i++) {
     botonEditar[i].onclick = () => {
@@ -27,23 +48,25 @@ for (let i = 0; i < botonEditar.length; i++) {
         return abrirModal();
     }
 };
-// funcionalidad de boton editar modal
+
+
 
 // funcionalidad de boton eliminar modal
 for (let i = 0; i < botonEliminar.length; i++) {
     botonEliminar[i].onclick = () => {
         // console.log("funciona boton eliminar")
         tituloVentanaModal.textContent = "Eliminar venta";
-        return abrirModal();
+        return abrirModalEliminar();
     }
 };
-// funcionalidad de boton eliminar modal
 
-fondoModal.onclick = () => {
+
+
+botonCancelarEditar.onclick = () => {
     return cerrarModal();
 }
 
-// console.log(ventas); ARRAY BIDIMENSIONAL
-// console.log(vendedoras); ARRAY COMUN
-// console.log(precios); ARRAY BIDIMENSIONAL
+botonCancelarEliminar.onclick = () => {
+    return cerrarModalEliminar();
+}
 
